7671c31a6912ee354ecb7753da35321d76a317c3
A. Saepudin <saepudinjeh@gmail.com>
2021-02-02 17:29:17 +0700

    fix bug

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(async (req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
-  error.status = error.status || 500;
+  error.status = error.status || 500 || 503;
   res.status(error.status);
   res.send({
     error: {
